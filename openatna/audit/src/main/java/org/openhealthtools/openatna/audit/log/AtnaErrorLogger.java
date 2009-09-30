daafbd9d0e19b0d1cb564e4cef644aacbe364118
andrew <andrew@c24ccc7d-296f-0410-8f60-a2dc5e63a092>
2009-09-30 18:25:28 +0000

    audit module

---
+++ openatna/audit/src/main/java/org/openhealthtools/openatna/audit/log/AtnaErrorLogger.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2009 University of Cardiff and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * Cardiff University - intial API and implementation
+ */
+
+package org.openhealthtools.openatna.audit.log;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.openhealthtools.openatna.anom.AtnaException;
+import org.openhealthtools.openatna.anom.AtnaMessage;
+
+/**
+ * This class logs errors at the ATNA message parsing layer.
+ *
+ *
+ * @author Andrew Harrison
+ * @version $Revision:$
+ * @created Sep 6, 2009: 11:26:36 AM
+ * @date $Date:$ modified by $Author:$
+ */
+
+public class AtnaErrorLogger {
+
+    static Log log = LogFactory.getLog("org.openhealthtools.openatna.audit.log.AtnaErrorLogger");
+
+    public static void log(AtnaException e) {
+        StringBuilder sb = new StringBuilder("===> ATNA EXCEPTION THROWN\n");
+        AtnaException.AtnaError error = e.getError();
+        sb.append("** ATNA ERROR:").append(error).append("**\n");
+        AtnaMessage msg = e.getAtnaMessage();
+        if (msg == null) {
+            sb.append("no message available.\n");
+        } else {
+            sb.append("message is:\n")
+                    .append(msg);
+        }
+        log.error(sb.toString(), e);
+    }
+
+}
---
+++ openatna/audit/src/main/java/org/openhealthtools/openatna/audit/process/ProcessContext.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2009 University of Cardiff and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * Cardiff University - intial API and implementation
+ */
+
+package org.openhealthtools.openatna.audit.process;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.openhealthtools.openatna.anom.AtnaMessage;
+
+/**
+ * @author Andrew Harrison
+ * @version $Revision:$
+ * @created Sep 13, 2009: 10:31:12 PM
+ * @date $Date:$ modified by $Author:$
+ */
+
+public class ProcessContext {
+
+    public static enum State {
+        INITIALIZED,
+        VALIDATED,
+        PERSISTED
+    }
+
+    private AtnaMessage message;
+    private State state;
+    private Map<String, Object> properties = new HashMap<String, Object>();
+
+    public ProcessContext(AtnaMessage message) {
+        this.message = message;
+        this.state = State.INITIALIZED;
+    }
+
+    public AtnaMessage getMessage() {
+        return message;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void putProperty(String key, Object value) {
+        properties.put(key, value);
+    }
+
+    public Object getProperty(String key) {
+        return properties.get(key);
+    }
+
+    public <T> T getProperty(String key, Class<? extends T> cls) {
+        Object val = properties.get(key);
+        if(val != null && cls.isAssignableFrom(val.getClass())) {
+            return (T)val;
+        }
+        return null;
+    }
+
+}
