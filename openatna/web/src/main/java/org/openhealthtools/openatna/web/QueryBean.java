2276c819f69b91c7dde4eb6fdf70983f1b830420
andrew <andrew@c24ccc7d-296f-0410-8f60-a2dc5e63a092>
2010-01-19 22:37:10 +0000

    simple web interface to query the repository

---
+++ openatna/web/src/main/java/org/openhealthtools/openatna/web/QueryBean.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2009 University of Cardiff and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * Cardiff University - intial API and implementation
+ */
+
+package org.openhealthtools.openatna.web;
+
+/**
+ * @author Andrew Harrison
+ * @version 1.0.0
+ * @date Jan 19, 2010: 11:48:17 AM
+ */
+
+public class QueryBean {
+
+    private String sourceId;
+    private String objectId;
+    private String participantId;
+    private String eventOutcome;
+    private String eventIdCode;
+    private String eventTypeCode;
+    private String eventAction;
+    private String eventTime;
+
+    public String getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(String sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(String participantId) {
+        this.participantId = participantId;
+    }
+
+    public String getEventOutcome() {
+        return eventOutcome;
+    }
+
+    public void setEventOutcome(String eventOutcome) {
+        this.eventOutcome = eventOutcome;
+    }
+
+    public String getEventIdCode() {
+        return eventIdCode;
+    }
+
+    public void setEventIdCode(String eventIdCode) {
+        this.eventIdCode = eventIdCode;
+    }
+
+    public String getEventAction() {
+        return eventAction;
+    }
+
+    public void setEventAction(String eventAction) {
+        this.eventAction = eventAction;
+    }
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getEventTypeCode() {
+        return eventTypeCode;
+    }
+
+    public void setEventTypeCode(String eventTypeCode) {
+        this.eventTypeCode = eventTypeCode;
+    }
+}
